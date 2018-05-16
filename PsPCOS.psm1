9459db4abcb4cdd9d781ad97c9a0b56f976521c8
sxlang1 <38787727+sxlang1@users.noreply.github.com>
2018-05-16 13:49:08 +0000

    Addition of ConvertTo-KMG to PsPCOS
    Added the filter (function) ConvertTo-KMG to PsPCOS module. This is to be used in future scripts.

--- PsPCOS.psm1
+++ PsPCOS.psm1
@@ -50,4 +50,28 @@ function Set-D9Creds
     {
         Write-Host "Credentials for $($D9Creds.UserName) successfully set to `$D9Creds object." -ForegroundColor Green
     }
+}
+
+　
+filter ConvertTo-KMG {
+<#
+.SYNOPSIS
+Converts byte counts to Byte\KB\MB\GB\TB\PB format
+.DESCRIPTION
+Accepts an [int64] byte count, and converts to Byte\KB\MB\GB\TB\PB format
+with decimal precision of 2
+.EXAMPLE
+3000 | convertto-kmg
+#>
+
+$bytecount = $_
+switch ([math]::truncate([math]::log($bytecount,1024))) 
+{
+    0 {"$bytecount Bytes"}
+    1 {"{0:n2} KB" -f ($bytecount / 1kb)}
+    2 {"{0:n2} MB" -f ($bytecount / 1mb)}
+    3 {"{0:n2} GB" -f ($bytecount / 1gb)}
+    4 {"{0:n2} TB" -f ($bytecount / 1tb)}
+    default {"{0:n2} PB" -f ($bytecount / 1pb)}
+}
 } 
